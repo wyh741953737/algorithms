783e8d3c4920c56c04306a772b13d110bef80f4c
wyh741953737 <741953737@qq.com>
2021-04-15 20:24:12 +0800

    双指针

---
+++ 队列/反转字符串.js
@@ -0,0 +1,9 @@
+// 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+// 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+function reverse(s) {
+    const n = s.length;
+    for (let left = 0, right = n - 1; left < right; ++left, --right) {
+        [s[left], s[right]] = [s[right], s[left]];
+    }
+    return s
+};
